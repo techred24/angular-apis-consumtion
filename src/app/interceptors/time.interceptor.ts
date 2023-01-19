0628f00673174b1ecd6d64d701d3ea2c67f905c1
Rafael <f.rafael.temich24@gmail.com>
2023-01-19 12:49:29 -0600

    Se agrega contexto a un interceptor

--- src/app/interceptors/time.interceptor.ts
+++ src/app/interceptors/time.interceptor.ts
@@ -3,24 +3,35 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpContext,
+  HttpContextToken
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
+const CHECK_TIME = new HttpContextToken<boolean>(() => false);
+export function checkTime() {
+  return new HttpContext().set(CHECK_TIME, true);
+}
 @Injectable()
 export class TimeInterceptor implements HttpInterceptor {
 
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const start = performance.now();
-    return next.handle(request)
-      .pipe(
-        tap(() => {
-          const time = (performance.now() - start) + 'ms';
-          console.log(request.url, time);
-        })
-      )
+    console.log(request.context, 'CONTEXT');
+    console.log(request.context.get(CHECK_TIME), 'CONTEXT GET');
+    if (request.context.get(CHECK_TIME)) {
+      const start = performance.now();
+      return next.handle(request)
+        .pipe(
+          tap(() => {
+            const time = (performance.now() - start) + 'ms';
+            console.log(request.url, time);
+          })
+        )
+    }
+    return next.handle(request);
   }
 }
